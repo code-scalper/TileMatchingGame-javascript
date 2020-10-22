9726678b1f3197276256bc91073e032577cf951b
HCC\junwoo.l <junwoo@hanwha.com>
2020-10-22 15:04:24 +0900

    'master'

--- js/flip.js
+++ js/flip.js
@@ -1,4 +1,4 @@
-import Tile from "./tile.js";
+// import Tile from "./tile.js";
 
 const images = [
   "circle",
@@ -13,13 +13,60 @@ const images = [
   "ui",
 ];
 
-const tiles = new Tile(images);
+// const tiles = new Tile(images);
 const container = document.querySelector(".card-ul");
-const tilesNodes = tiles.makeTileNodes();
-const tilesNodesClone = tiles.makeTileNodes();
 
-tilesNodes.forEach((tile) => container.appendChild(tile));
-tilesNodesClone.forEach((tile) => container.appendChild(tile));
+
+makeTiles()
+
+// 타일만들기
+function makeTiles(){
+  const doubledImages = shuffle([...images,...images]);
+  const list = makeList(doubledImages)
+  list.forEach( li => container.appendChild(li))
+}
+
+// 배열 섞기
+function shuffle(array) {
+  let index = array.length - 1;
+  while (index > 0) {
+      let randomIndex = Math.floor(Math.random() * (index + 1));
+      [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+      index--;
+  }
+  return array;
+}
+
+// li node 만들기
+function makeList(imageArray){
+  const listItem = imageArray.map((image) => {
+      const li = document.createElement("li");
+      li.classList.add("card-li");
+      li.setAttribute("data-type", image);
+
+      const flipContainer = document.createElement("div");
+      flipContainer.classList.add("flip-container");
+
+      const coverSpan = document.createElement("span");
+      coverSpan.classList.add("cover");
+
+      const flippingSpan = document.createElement("span");
+      flippingSpan.classList.add("cover-flipped");
+
+      const img = document.createElement("img");
+      img.setAttribute("src", `images/${image}.png`);
+      img.setAttribute("alt", image);
+      flippingSpan.appendChild(img);
+
+      flipContainer.appendChild(coverSpan);
+      flipContainer.appendChild(flippingSpan);
+
+      li.appendChild(flipContainer);
+      return li;
+    });
+
+    return listItem;
+}
 
 // game
 let selectedCard;
@@ -29,8 +76,9 @@ const cards = document.querySelectorAll(".card-li");
 
 cards.forEach((card) => {
   card.addEventListener("click", (e) => {
-    const flipContainer = card.children;
-    flipContainer[0].classList.add("clicked-container");
+    const flipContainer = card.children[0];
+    console.log(flipContainer)
+    flipContainer.classList.add("clicked-container");
     console.log(card.getAttribute("data-type"));
   });
 });
