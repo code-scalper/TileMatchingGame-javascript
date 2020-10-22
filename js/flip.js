3a8527fa954b1b8bcc7085e563135e35e82afb24
HCC\junwoo.l <junwoo@hanwha.com>
2020-10-22 16:56:14 +0900

    'finish'

--- js/flip.js
+++ js/flip.js
@@ -13,12 +13,29 @@ const images = [
   "ui",
 ];
 
+// game
+let selectedCard = null;
+let selectedIndex = null;
+let completedItems = [];
+let time = 0;
+let timeInterval = null;
+
 // const tiles = new Tile(images);
 const container = document.querySelector(".card-ul");
+const startButton = document.querySelector(".start-button");
+const history = document.querySelector(".history");
+const timeDisplay = document.querySelector(".timeDisplay");
 
 
-makeTiles()
 
+startButton.addEventListener("click",()=>{
+  container.innerHTML = ""
+  completedItems = [];
+  selectedCard = null;
+  selectedIndex = null;
+  makeTiles()
+  gameService()
+})
 // 타일만들기
 function makeTiles(){
   const doubledImages = shuffle([...images,...images]);
@@ -68,17 +85,53 @@ function makeList(imageArray){
     return listItem;
 }
 
-// game
-let selectedCard;
-let selectedIndex;
 
-const cards = document.querySelectorAll(".card-li");
 
-cards.forEach((card) => {
-  card.addEventListener("click", (e) => {
-    const flipContainer = card.children[0];
-    console.log(flipContainer)
-    flipContainer.classList.add("clicked-container");
-    console.log(card.getAttribute("data-type"));
+function gameService(){
+  const cards = document.querySelectorAll(".card-li")
+  cards.forEach((card ,index) => {
+    card.addEventListener("click", (e) => {
+      if(completedItems.includes(index)) return;
+      card.classList.add("selected-li");
+      const name = card.getAttribute("data-type") 
+      if(selectedCard === name && selectedIndex !== index ){
+        completedItems = [...completedItems, selectedIndex, index];
+        cards[index].classList.add("done-li")
+        cards[selectedIndex].classList.add("done-li")
+        selectedCard = null;
+        selectedIndex = null;
+
+        const isCompleted = completedItems.length === cards.length;
+        if(isCompleted){
+          clearInterval(timeInterval);
+          const historyLi = document.createElement("li");
+          historyLi.innerText = time.toFixed(1);
+          history.appendChild(historyLi)
+        }
+
+      } else {
+        // 카드 뒤집기 초기화
+        if(selectedIndex){
+          cards[index].classList.remove("selected-li")
+          cards[selectedIndex].classList.remove("selected-li")
+        }
+        
+        const isSelected = selectedCard === null;
+        if(isSelected){
+          selectedCard = name
+          selectedIndex = index
+        } else {
+          selectedCard = null
+          selectedIndex = null
+        }
+      }
+    });
   });
-});
+  // interval 
+  timeInterval = setInterval(()=>{
+    time = time + 0.1;
+    timeDisplay.innerText = time.toFixed(1);
+  },100)
+}
+
+
